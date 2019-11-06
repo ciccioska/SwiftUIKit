546c8b12cbd56e96d4fd5e5c1cffcd48ac9bdeb6
zmeriksen <zmeriksen@gmail.com>
2019-11-05 19:08:22 -0600

    Testing defer

--- Sources/SwiftUIKit/Extensions/UIView+SwiftUIKit.swift
+++ Sources/SwiftUIKit/Extensions/UIView+SwiftUIKit.swift
@@ -118,7 +118,9 @@ public extension UIView {
     @discardableResult
     func width(equalTo view: UIView, multiplier: Float = 1, constant: Float = 1) -> Self {
         
-        widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: CGFloat(multiplier), constant: CGFloat(constant)).isActive = true
+        defer {
+            widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: CGFloat(multiplier), constant: CGFloat(constant)).isActive = true
+        }
         
         return self
     }
